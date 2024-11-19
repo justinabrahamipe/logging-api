c9c15f60479ecb7d88b2c79f37a92f8aa732546d
Justin Abraham Ipe <jaik@JUSTIN-LAPTOP>
2024-11-19 16:21:32 +0000

    notion is connected to the backend

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from 'cors';
 import dotenv from "dotenv";
 import { Express } from "express";
 import router from "./configs/routes";
@@ -6,10 +7,10 @@ import router from "./configs/routes";
 dotenv.config();
 
 const app: Express = express();
-const port = 3000;
-
+const port = process.env.PORT;
+app.use(cors());
 app.use("/",router);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`);
+  console.log(`Example app listening on port ${port}`);
 });
