ad908143188aebabbe14b6ac2132ffff3693f737
justinabrahamipe <justinabrahamipe@gmail.com>
2024-11-29 15:53:28 +0000

    get goals is added

--- configs/routes.ts
+++ configs/routes.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from "express";
 import { getUsers } from "../src/getActivitiesList";
+import {getGoalsList} from '../src/getGoalsList'
 import {createNewLogEntry } from "../src/createNewLogEntry";
 
 
@@ -16,6 +17,15 @@ router.get("/activities", (req: Request, res: Response) => {
     }
   );
 });
+
+router.get("/goals", (req: Request, res: Response) => {
+  getGoalsList(process.env.NOTION_TOKEN || "", process.env.GOALS_DB || "").then(
+    (data) => {
+      res.send(data?.results);
+    }
+  );
+});
+
 
 router.get("/createNewLogEntry", (req: Request, res: Response) => {
   createNewLogEntry(process.env.NOTION_TOKEN || "", process.env.ACTIVITES_DB || "").then(
